80e0926262419c429953bf91b3f3827ce7d9f0b1
Furkan YİĞİT <117458984+yigitfurkann@users.noreply.github.com>
2026-04-21 15:39:16 +0300

    Update app.py

--- clous/app.py
+++ clous/app.py
@@ -109,4 +109,4 @@ if __name__ == '__main__':
     print("  Clous.cloud — Huawei MaaS Interface")
     print("  → http://localhost:5000")
     print()
-    app.run(debug=True, port=5000, threaded=True)
+    app.run(host='0.0.0.0', port=5000, debug=True, threaded=True)
